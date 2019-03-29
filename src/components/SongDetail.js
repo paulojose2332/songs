224ac41f7df4a400729b6979be3ceb48af7dfdef
paulojose2332 <paulo.jose.n@outlook.com>
2019-03-29 14:50:34 -0300

    commit

---
+++ src/components/SongDetail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {connect} from 'react-redux'
+
+const SongDetail = ({song}) => {
+    if(!song){
+        return <div> select a song</div>
+    }
+    return(
+        <div>
+            <h3>Details for:</h3>
+            <p>Title {song.title} <br/>Duration {song.duration}</p>
+        </div>  
+    );
+};
+
+const mapStateToProps = props => {
+    return {song: props.selectedSong}
+};
+
+export default connect(mapStateToProps)(SongDetail)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,7 +7,7 @@ const songsReducer = () => {
     return [
         {title: 'No Scrubs', duration: '4:05'},
         {title: 'Sound of Silence', duration: '5:03'},
-        {title: 'I want it that way' , duration: '3,17'},
+        {title: 'I want it that way' , duration: '3:17'},
         {title: 'Wind of change', duration: '6:27'}
     ];
 };
